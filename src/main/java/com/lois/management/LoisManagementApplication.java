8c321bc70a5ed8ba05383ce108fef54d1aff6ab8
zzinobak <kimha0094@gmail.com>
2025-11-03 12:20:31 +0900

    Initial commit: lois-management Spring Boot project

---
+++ src/main/java/com/lois/management/LoisManagementApplication.java
@@ -0,0 +1,13 @@
+package com.lois.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoisManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoisManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lois/management/LoisManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lois.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoisManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
