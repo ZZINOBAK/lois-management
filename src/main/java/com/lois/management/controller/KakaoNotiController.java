df40feda5375c8de6185445a977cd0c8b4b472a3
zzinobak <kimha0094@gmail.com>
2025-11-29 10:23:55 +0900

    수정

--- src/main/java/com/lois/management/controller/KakaoNotiController.java
+++ src/main/java/com/lois/management/controller/KakaoNotiController.java
@@ -1,4 +1,4 @@
-package com.lois.management.controller;
+//package com.lois.management.controller;
 
 import com.lois.management.service.KakaoNotiService;
 import lombok.RequiredArgsConstructor;
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/kakao")
-@RequiredArgsConstructor
-public class KakaoNotiController {
-
-    private final KakaoNotiService kakaoNotiService;
-
-    /**
-     * 알림톡 한건 발송 예제
-     */
-    @PostMapping("/send-one-ata")
-    public SingleMessageSentResponse sendOneAta() {
-        
-        return kakaoNotiService.sendOneAta();
-    }
-}
+//@RestController
+//@RequestMapping("/kakao")
+//@RequiredArgsConstructor
+//public class KakaoNotiController {
+//
+//    private final KakaoNotiService kakaoNotiService;
+//
+//    /**
+//     * 알림톡 한건 발송 예제
+//     */
+//    @PostMapping("/send-one-ata")
+//    public SingleMessageSentResponse sendOneAta() {
+//
+//        return kakaoNotiService.sendOneAta();
+//    }
+//}
